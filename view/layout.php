71ac63534d51e0094884d398e4c4ccee03cb3c5f
Enrique <enrikeniet90@gmail.com>
2026-01-11 19:09:47 +0100

    fix[backend]: modificaciones fuentes css del layout

--- view/layout.php
+++ view/layout.php
@@ -10,7 +10,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Aplicación LoginLogoff | Enrique Nieto</title>
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="webroot/css/all.min.css">
     <link rel="stylesheet" href="webroot/css/estilosLogin.css?v=2">
 </head>
 <body>
